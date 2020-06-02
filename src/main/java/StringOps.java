6bd71a98804d5275cd7b8d1b55373921016018da
marishul <mariashul95@gmail.com>
2020-06-02 16:27:00 +0300

    comments commit

--- src/main/java/StringOps.java
+++ src/main/java/StringOps.java
@@ -3,13 +3,13 @@ public class StringOps {
     public static void main(String[] args) {
         String str1 = "Songs are our weapons, our power of powers,\nOur gold — our voices — just hear us sing!\n";
         String str2 = "Meadow, lie green on the earth!\nWith silk our days for us line!";
-        String str3 = (str1.concat(str2));
+        String str3 = (str1.concat(str2));  //Strings concatenation
 
-        System.out.println(str2.indexOf('s', 5));
-        System.out.println(str3.indexOf("weapons"));
+        System.out.println(str2.indexOf('s', 5)); //Finding index of character
+        System.out.println(str3.indexOf("weapons")); //Finding index of substring
         System.out.print("\n");
 
-        if ((str1.length() + str2.length() == str3.length())) {
+        if ((str1.length() + str2.length() == str3.length())) { //Determine strings length and comparison
             System.out.println("Concatenation provides the same length");
             System.out.print("\n");
         } else {
@@ -17,11 +17,11 @@ public class StringOps {
             System.out.print("\n");
         }
 
-        System.out.println(str3.substring(14, 21));
+        System.out.println(str3.substring(14, 21)); //Extracting substring
         System.out.print("\n");
 
-        System.out.println(str1.replaceAll("weapons", "Bazinga!"));
+        System.out.println(str1.replaceAll("weapons", "Bazinga!")); //Replacing substring
 
-        System.out.println((str1.concat(str2)).equals(str3));
+        System.out.println((str1.concat(str2)).equals(str3)); //Strings equality
     }
 }
